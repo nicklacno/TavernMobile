02b0e384d1b5f0f0a86ceb9ef499dbe322527093
Nicholas Lacno <kailuanick@gmail.com>
2024-01-26 11:27:23 -0800

    Add project files.

---
+++ WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private ILogger<ProfileController> _logger;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetProfile")]
+        public IEnumerable<Profile> Get(int i)
+        {
+
+        }
+    }
+}
