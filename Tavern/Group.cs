8fab81f3c33f62627f2742d3818b53cec771ec8c
Nicholas Lacno <kailuanick@gmail.com>
2024-02-25 10:42:45 -0800

    Added a Group Page and Group class for the app

---
+++ Tavern/Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavern
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        public List<string> Tags { get; set; }
+        public List<string> Members { get; set; }
+
+
+        public Group(int id, string name, string bio)
+        {
+            Id = id;
+            Name = name;
+            Bio = bio;
+        }
+
+
+    }
+}
---
+++ Tavern/GroupPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Tavern;
+
+public partial class GroupPage : ContentPage
+{
+	public GroupPage()
+	{
+		InitializeComponent();
+	}
+}
