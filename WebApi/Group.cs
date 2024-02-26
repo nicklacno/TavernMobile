472da73d076c503786b883c84f0cd090c70e6e86
Nicholas Lacno <kailuanick@gmail.com>
2024-02-26 10:27:37 -0800

    Api for Group members and tags are now added to groups

--- WebApi/Group.cs
+++ WebApi/Group.cs
@@ -7,10 +7,11 @@ namespace WebApi
         private static string? connectionString = null;
 
         public int GroupId { get; set; }
-        public string Name { get; set; }
-        public string Bio { get; set; }
+        public string? Name { get; set; }
+        public string? Bio { get; set; }
         public int OwnerId { get; set; }
-        public List<string> members { get; set; }
+        public List<string>? members { get; set; }
+        public List<string>? tags { get; set; }
 
         /**
          * GetGroup - Returns the group given the specific id
@@ -37,6 +38,9 @@ namespace WebApi
                             group.OwnerId = reader.GetInt32(0);
                             group.Name = reader.GetString(1);
                             group.Bio = reader.GetString(2);
+
+                            group.members = GetMembers(id);
+                            group.tags = GetTags(id);
                         }
                     }
                 }
@@ -56,10 +60,64 @@ namespace WebApi
          */
         private static List<string>? GetMembers(int id) 
         {
+            SetConnectionString();
             try
             {
                 List<string> members = new List<string>();
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand()) 
+                    {
+                        cmd.CommandText = "SELECT UserName FROM MemberGroup JOIN Customers " +
+                                            "ON MemberGroup.UserID = Customers.UserID " +
+                                            "WHERE GroupID = @GroupP";
+                        cmd.Parameters.AddWithValue("@GroupP", id);
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read()) 
+                        {
+                            members.Add(reader.GetString(0));
+                        }
+                    }
+                }
                 return members;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /**
+         * GetTags - helper method that retrieves the data from the database
+         * @param id - Group Id the members are apart of
+         * @return - the list of names of the group members, null if error
+         */
+        private static List<string>? GetTags(int id)
+        {
+            SetConnectionString();
+            try
+            {
+                List<string> tags = new List<string>();
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT Name FROM GroupTags " +
+                                            "JOIN Tags ON Tags.TagID = GroupTags.TagID " +
+                                            "WHERE GroupID = @GroupP;";
+                        cmd.Parameters.AddWithValue("@GroupP", id);
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            tags.Add(reader.GetString(0));  
+                        }
+                    }
+                }
+                return tags;
             }
             catch (Exception ex)
             {
