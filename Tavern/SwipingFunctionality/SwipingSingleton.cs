6a6239bfc25d3e860fe95537c102dfd8db13f203
Gage McNeil <mcneil_gage@yahoo.com>
2024-04-28 17:33:46 -0700

    Minor changes to swiping singleton, I can also now test the code sadly

--- Tavern/SwipingFunctionality/SwipingSingleton.cs
+++ Tavern/SwipingFunctionality/SwipingSingleton.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Tavern.SwipingFunctionality
 {
-    public static class SwipingSingleton
+    public class SwipingSingleton
     {
         public static Queue<Group> Groups = new Queue<Group>();
         public delegate void GroupAction();
-
         public static GroupAction SkipGroup;
         public static GroupAction RequestGroup;
-
+        public Group LikedGroup { get; set; }
+        private readonly HttpClient _httpClient = new(); //creates client
+        private const string BASE_ADDRESS = "https://cxbg938k-7111.usw2.devtunnels.ms/"; //base address for persistent dev-tunnel for api
     }
 }
