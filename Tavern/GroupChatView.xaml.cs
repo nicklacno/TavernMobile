101ff5c33b242776228225477b932d18a3fe5259
nicklacno <kailuanick@gmail.com>
2024-04-29 12:08:33 -0700

    Now shows chat in group page. Needs code to send messages

--- Tavern/GroupChatView.xaml.cs
+++ Tavern/GroupChatView.xaml.cs
@@ -18,5 +18,24 @@ public partial class GroupChatView : ContentView
     private async Task AddMessages(int groupId)
     {
         List<Dictionary<string, string>> messages = await ProfileSingleton.GetInstance().GetMessages(groupId);
+        if (messages != null)
+        {
+            foreach(var message in messages)
+            {
+                HorizontalStackLayout stack = new HorizontalStackLayout();
+                var lb = new Label();
+                DateTime time = Convert.ToDateTime(message["timestamp"]).ToLocalTime();
+                lb.Text = time.ToShortTimeString();
+                stack.Add(lb);
+                lb = new Label();
+                lb.Text = " "+message["sender"] + ":";
+                stack.Add(lb);
+                lb = new Label();
+                lb.Text = message["message"];
+                stack.Add(lb);
+
+                messageBox.Add(stack);
+            }
+        }
     }
 }
