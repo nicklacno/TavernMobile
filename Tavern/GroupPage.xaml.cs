3f1d0cccd5427b57f9591beec6981489a8343743
Gage McNeil <mcneil_gage@yahoo.com>
2024-05-13 10:30:10 -0700

    Appearance/bug fixes

--- Tavern/GroupPage.xaml.cs
+++ Tavern/GroupPage.xaml.cs
@@ -72,6 +72,8 @@ public partial class GroupPage : ContentPage
 		{
 			Button btn = new Button();
 			btn.Text = "Edit Group";
+			btn.FontFamily = "Sedan";
+			btn.FontSize = 35;
 			btn.Clicked += PushGroupPage;
 
 			stackInfo.Insert(4, btn);
